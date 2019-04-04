2d0812b7ebac80b6d7b0ec9a2199c754dba2b577
oms1218ju <oms1218ju@naver.com>
2019-04-04 11:43:15 +0900

    20190328

---
+++ 20190328/src/TO_DO_7.java
@@ -0,0 +1,17 @@
+
+public class TO_DO_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=0;i<10;i++) {
+			System.out.print("sum="+sum);
+			sum = sum + i;
+			System.out.println(", i="+i+", sum="+sum);
+		}
+		
+		System.out.println("гую╨ "+sum);
+
+	}
+
+}
