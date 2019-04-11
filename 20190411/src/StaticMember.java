af097255e8bc810ffd813c65903ff702013e9701
oms1218ju <oms1218ju@naver.com>
2019-04-11 19:50:37 +0900

    20190411_3

--- 20190411/src/StaticMember.java
+++ 20190411/src/StaticMember.java
@@ -1,39 +1,40 @@
 import java.util.Scanner;
 
 class CurrencyConverter{
-	protected int exchangeRate;
-	protected int price;
-	public CurrencyConverter(int exchangeRate2, int num2) {
-		// TODO Auto-generated constructor stub
+	protected static int exchangeRate;
+	
+	public CurrencyConverter(int num) {
+		exchangeRate = num;
 	}
-	void exchangeSet(int exchangeRate,int price){
-		this.exchangeRate = exchangeRate;
-		this.price = price;
+	
+	double toDallar(int num) {
+		return (double)num/exchangeRate;
 	}
-	double toDallar() {
-		return price/exchangeRate;
-	}
-	float toKWR() {
-		return price*exchangeRate;
+	float toKWR(int num) {
+		return num*exchangeRate;
 	}
 	
 }
 public class StaticMember {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num = 0,exchangeRate=0;
+
+		int exchangeRate;
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("환율을 입력하세요.>>");
 		exchangeRate = scanner.nextInt();
 		scanner.close();
+		
+		
 		System.out.println("백만원에 대해 계산");
-		num=1000000;
-		CurrencyConverter cu = new CurrencyConverter(exchangeRate, num);
-		System.out.println("백만원은 $"+cu.toDallar()+"입니다.");
-		num=100;
-		CurrencyConverter gs = new CurrencyConverter(exchangeRate, num);
-		System.out.println("$"+gs.price+"는"+gs.toKWR()+"원 입니다.");
+		
+		CurrencyConverter cu = new CurrencyConverter(exchangeRate);
+		
+		System.out.println("백만원은 $"+cu.toDallar(1000000)+"입니다.");
+		
+		CurrencyConverter gs = new CurrencyConverter(exchangeRate);
+		System.out.println("$100는"+gs.toKWR(100)+"원 입니다.");
 
 	}
 
